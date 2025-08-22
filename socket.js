c27ee47e5269cfbaf64ece2d1aff3da8771104ed
ishmeetwaplia <ishmeetsingh@waplia.in>
2025-08-22 16:46:31 +0530

    Create delete bet game socket

--- socket.js
+++ socket.js
@@ -1,17 +1,34 @@
 const { Server } = require("socket.io");
+const Game = require("./src/models/Game");
 
 function initSocket(server) {
   const io = new Server(server, {
     cors: { origin: "http://localhost:3000" },
   });
 
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     console.log("User connected:", socket.id);
-    
-    socket.on("sendMessage", (data) => {
-      console.log("Message received:", data);
-      io.emit("receiveMessage", data);
-    });
+
+    try {
+      setInterval(async () => {
+        try {
+          const updatedGames = await Game.find({ status: "pending" }).populate("createdBy", "username credit");
+          io.emit("games_list", updatedGames);
+        } catch (error) {
+          console.error("Error fetching games:", error);
+        }
+      }, 5000);
+    } catch (error) {
+      console.error("Error fetching games:", error);
+    }
+
+    socket.on("delete_game", async (gameId) => {
+      try {
+        await Game.findByIdAndDelete(gameId);
+      } catch (error) {
+        console.error("Error deleting games:", error);
+      }
+    })
 
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
--- src/models/Game.js
+++ src/models/Game.js
@@ -1,9 +1,29 @@
 const mongoose = require("mongoose");
 
 const GameSchema = new mongoose.Schema({
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  betAmount: { type: Number, required: true },
-  winningAmount: { type: Number, required: true }, // new field
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  acceptedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null
+  },
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "completed", "cancelled", "expired"],
+    default: "pending"
+  },
+  betAmount: {
+    type: Number,
+    required: true
+  },
+  winningAmount: {
+    type: Number,
+    required: true
+  },
 }, { timestamps: true, versionKey: false });
 
 module.exports = mongoose.model("Game", GameSchema);
