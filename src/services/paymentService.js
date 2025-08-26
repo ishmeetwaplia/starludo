3c28d648027bb4fac9c984645535db8fd5151afc
ishmeetwaplia <ishmeetsingh@waplia.in>
2025-08-26 11:43:32 +0530

    Changes

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -25,7 +25,6 @@ exports.createPayment = async (req) => {
     });
 
     if (global.io) {
-      global.io.emit("pending_payments_list", await Payment.find({ status: "pending" }));
       global.io.emit("new_payment", payment);
     }
 
